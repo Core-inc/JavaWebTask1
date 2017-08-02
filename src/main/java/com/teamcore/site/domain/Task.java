cf7ed7d41cfd519e16b5935ad2ed213a210354ce
alterg <alterg@niuitmo.ru>
2017-08-02 14:34:51 +0300

    Task domain
    According our db

--- src/main/java/com/teamcore/site/domain/Task.java
+++ src/main/java/com/teamcore/site/domain/Task.java
@@ -1,13 +1,37 @@
 package com.teamcore.site.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
 
 /**
- * Created by igoz on 26.07.17.
+ * Created by alterG on 1.08.17.
  */
 
 @Entity
 @Table(name = "t_tasks")
 public class Task extends AbstractDomainClass {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private long cost;
+
+    @Column
+    private long duration;
+
+    @Column
+    private int status;
+
+    @Column(nullable = false)
+    private DateTime createdAt;
+
+    @Column(nullable = false)
+    private DateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "projectId")
+    private Project project;
+
 }

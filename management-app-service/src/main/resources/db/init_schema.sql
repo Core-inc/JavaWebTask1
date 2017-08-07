9d9edc4989ff0970997b4d13d6c229b9613a232a
Igor Vasiliev <igoz@users.noreply.github.com>
2017-08-07 23:48:33 +0300

    Update init_schema.sql

--- management-app-service/src/main/resources/db/init_schema.sql
+++ management-app-service/src/main/resources/db/init_schema.sql
@@ -13,7 +13,7 @@ create table t_users (
 	c_password   		varchar(255) not null,
 	c_salt       		varchar(255) not null,
 	c_created_at 		timestamp DEFAULT current_timestamp not null,
-	c_updated_at 		timestamp DEFAULT current_timestamp not null,
+	c_updated_at 		timestamp DEFAULT current_timestamp null,
 	c_user_group_id 	integer not null,
 	FOREIGN KEY (c_user_group_id) REFERENCES t_user_groups(c_group_id)
 );
@@ -22,7 +22,7 @@ create table t_users (
 create table t_developers (
 	id				serial primary key not null,
 	c_user_id			integer not null unique,
-	c_skill_level 		integer not null,	
+	c_skill_level 		integer not null,
 	FOREIGN KEY (c_user_id) REFERENCES t_users(id)
 );
 
@@ -52,7 +52,7 @@ create table t_tasks (
 
 create table t_skills (
 	id			serial primary key not null,
-	c_name			text not null,
+	c_name			text not null
 );
 
 create table t_developers_tasks (
@@ -69,7 +69,7 @@ create table t_developers_skills (
 	c_developer_id 		integer not null,
 	c_skill_id 			integer not null,
 	FOREIGN KEY (c_developer_id) REFERENCES t_users(id),
-	FOREIGN KEY (c_skill_id) REFERENCES t_skills(id),
+	FOREIGN KEY (c_skill_id) REFERENCES t_skills(id)
 );
 
 create table t_tasks_skills (
@@ -77,5 +77,5 @@ create table t_tasks_skills (
 	c_task_id 		integer not null,
 	c_skill_id 			integer not null,
 	FOREIGN KEY (c_task_id) REFERENCES t_users(id),
-	FOREIGN KEY (c_skill_id) REFERENCES t_skills(id),
+	FOREIGN KEY (c_skill_id) REFERENCES t_skills(id)
 );

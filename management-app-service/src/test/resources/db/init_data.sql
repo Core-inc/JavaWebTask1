9ff27d4eda548cc236ec89ca44aa92603899e474
vasenka <sitnikova.va@mail.ru>
2017-08-09 09:29:22 +0300

    adding inserts in testdb

--- management-app-service/src/test/resources/db/init_data.sql
+++ management-app-service/src/test/resources/db/init_data.sql
@@ -7,3 +7,14 @@ insert into t_user_groups(c_group_id, c_name) values(
 
 insert into t_skills(c_name) values('Java');
 insert into t_skills(c_name) values('Python');
+
+
+insert into t_user_groups(c_group_id, c_name) values(
+0,'admin');
+
+INSERT INTO t_projects (c_exter_name, c_inter_name, c_specs_link, c_status, c_created_at, c_updated_at)
+      VALUES("testOuterName", "testInterName", "http://test", 0, "03-07-2017", "03-07-2017");
+INSERT INTO t_projects (c_exter_name, c_inter_name, c_specs_link, c_status, c_created_at, c_updated_at)
+      VALUES("Web-site", "Web-site-1", "http://web_site_1", 0, "05-07-2017", "05-07-2017");
+INSERT INTO t_projects (c_exter_name, c_inter_name, c_specs_link, c_status, c_created_at, c_updated_at)
+            VALUES("Game_tanks", "World_of_tanks", "http://world_of_tanks", 0, "07-07-2017", "07-07-2017");

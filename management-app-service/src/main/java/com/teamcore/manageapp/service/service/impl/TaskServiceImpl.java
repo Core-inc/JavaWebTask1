3d3e75e5b6504160f568aa69e30cb26926a0d6cc
alterg <alterg@niuitmo.ru>
2017-08-10 19:21:34 +0300

    TaskServiceImpl add methods implementation

--- management-app-service/src/main/java/com/teamcore/manageapp/service/service/impl/TaskServiceImpl.java
+++ management-app-service/src/main/java/com/teamcore/manageapp/service/service/impl/TaskServiceImpl.java
@@ -1,13 +1,75 @@
 package com.teamcore.manageapp.service.service.impl;
 
+import com.teamcore.manageapp.service.dao.TaskDAO;
+import com.teamcore.manageapp.service.domain.Project;
 import com.teamcore.manageapp.service.domain.Task;
 import com.teamcore.manageapp.service.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    private TaskService taskService;
+
+    @Autowired
+    private TaskDAO taskDAO;
+
+    // for mockito tests
+    public TaskServiceImpl(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    /**
+     * Find task by id
+     * @param id
+     * @return task with id
+     */
+    @Override
+    public Task findTaskById(long id) {
+        return taskDAO.findTaskById(id);
+    }
+
+    /**
+     * Delete task by id
+     * @param id of task to delete
+     */
+    @Override
+    public void deleteTaskById(long id) {
+        taskDAO.deleteTaskById(id);
+    }
+
+    /**
+     * update task in database
+     * @param task to update
+     */
+    @Override
+    public void updateTask(Task task) {
+        taskDAO.updateTask(task);
+    }
+
+    /**
+     * add task to database
+     * @param task to add
+     * @return task with id
+     */
+    @Override
+    public Task addTask(Task task) {
+        return taskDAO.addTask(task);
+    }
+
+    /**
+     * find all tasks by project
+     * @param project
+     * @return list of task of project
+     */
+    @Override
+    public List<Task> findAllTasksByProject(Project project) {
+        return taskDAO.findAllTasksByProject(project);
+    }
+
 
     @Override
     public List<? extends Task> getAll() {
@@ -16,21 +78,22 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Task getById(Long id) {
-        return null;
+        return findTaskById(id);
     }
 
     @Override
     public Task save(Task domainObject) {
-        return null;
+        return taskDAO.addTask(domainObject);
     }
 
     @Override
     public Task update(Task domainObject) {
-        return null;
+        taskDAO.updateTask(domainObject);
+        return domainObject;
     }
 
     @Override
     public void delete(Long id) {
-
+        taskDAO.deleteTaskById(id);
     }
 }

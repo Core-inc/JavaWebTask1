7558d444a9e91ac61d7a3d3e1f90daf6fd3ac2f2
Flynston <Flynston@l0wrider.noreply.github.com>
2017-08-12 16:05:32 +0300

    ProjectRequestServiceImpl imports fix

--- management-app-service/src/main/java/com/teamcore/manageapp/service/service/impl/ProjectRequestServiceImpl.java
+++ management-app-service/src/main/java/com/teamcore/manageapp/service/service/impl/ProjectRequestServiceImpl.java
@@ -1,18 +1,13 @@
 package com.teamcore.manageapp.service.service.impl;
 
-import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
-import com.teamcore.manageapp.service.dao.ProjectDAO;
 import com.teamcore.manageapp.service.dao.ProjectRequestDAO;
-import com.teamcore.manageapp.service.domain.Project;
 import com.teamcore.manageapp.service.domain.ProjectRequest;
 import com.teamcore.manageapp.service.service.ProjectRequestService;
-import com.teamcore.manageapp.service.service.ProjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.List;
 
 @Service

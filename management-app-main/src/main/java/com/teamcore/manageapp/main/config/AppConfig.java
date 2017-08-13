c95c42b1f98a9fda7dc045a81c7f51e822589bd3
Igor <igozzz94@gmail.com>
2017-08-13 17:18:43 +0300

    AppConfig basePackageCalsses fix

--- management-app-main/src/main/java/com/teamcore/manageapp/main/config/AppConfig.java
+++ management-app-main/src/main/java/com/teamcore/manageapp/main/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.teamcore.manageapp.main.config;
 
 import com.teamcore.manageapp.service.config.DatabaseConfig;
+import com.teamcore.manageapp.service.dao.DaoMarker;
+import com.teamcore.manageapp.service.service.ServiceMarker;
 import com.teamcore.manageapp.web.config.WebMvcConfig;
 import com.teamcore.manageapp.web.config.WebSecurityConfig;
 import com.teamcore.manageapp.web.controllers.ControllersMarker;
@@ -10,6 +12,6 @@ import org.springframework.context.annotation.Import;
 
 
 @Configuration
-@ComponentScan(basePackageClasses = {ControllersMarker.class})
+@ComponentScan(basePackageClasses = {DaoMarker.class, ServiceMarker.class, ControllersMarker.class})
 @Import({DatabaseConfig.class, WebSecurityConfig.class, WebMvcConfig.class})
 public class AppConfig { }

3dadf74827b853dc55d7bb40bd2cdf5eee2a209c
Igor <igozzz94@gmail.com>
2017-08-14 14:25:14 +0300

    add dev

--- management-app-web/src/main/java/com/teamcore/manageapp/web/controllers/ProjectManagementController.java
+++ management-app-web/src/main/java/com/teamcore/manageapp/web/controllers/ProjectManagementController.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/manager")
 public class ProjectManagementController {
-    @RequestMapping(value = "/project/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/cabinet/project/{id}", method = RequestMethod.GET)
     public String projectInfo() {
         return "/manager/project/project_info";
     }
 
-    @RequestMapping(value = "/project/{id}/add_task", method = RequestMethod.GET)
+    @RequestMapping(value = "/cabinet/project/{id}/add_task", method = RequestMethod.GET)
     public String addTask() {
         return "/manager/project/add_task";
     }
 
-    @RequestMapping(value = "/project/{id}/tech_task", method = RequestMethod.GET)
+    @RequestMapping(value = "/cabinet/project/{id}/tech_task", method = RequestMethod.GET)
     public String techTask() {
         return "/manager/project/tech_task";
     }
 
-    @RequestMapping(value = "/project/{id}/status", method = RequestMethod.GET)
+    @RequestMapping(value = "/cabinet/project/{id}/status", method = RequestMethod.GET)
     public String projectStatus() {
         return "/manager/project/task_info";
     }
